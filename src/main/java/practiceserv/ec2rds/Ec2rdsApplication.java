1dd0e340cf29978d8c7ecab9aebb748d58374f80
염경호 <162652059+KyunghoYeom@users.noreply.github.com>
2025-03-27 10:59:59 +0900

    test

---
+++ src/main/java/practiceserv/ec2rds/Ec2rdsApplication.java
@@ -0,0 +1,13 @@
+package practiceserv.ec2rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2rdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2rdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practiceserv/ec2rds/Ec2rdsApplicationTests.java
@@ -0,0 +1,13 @@
+package practiceserv.ec2rds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2rdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
