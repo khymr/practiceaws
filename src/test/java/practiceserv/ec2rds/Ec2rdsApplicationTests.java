eea319eb53bea360e39ca6fb60c102217a608ecd
염경호 <162652059+KyunghoYeom@users.noreply.github.com>
2025-03-27 13:18:27 +0900

    올바른 테스트

--- src/test/java/practiceserv/ec2rds/Ec2rdsApplicationTests.java
+++ src/test/java/practiceserv/ec2rds/Ec2rdsApplicationTests.java
@@ -3,11 +3,14 @@ package practiceserv.ec2rds;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class Ec2rdsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(1 + 1).isEqualTo(2); // ✅ 항상 성공하는 테스트
 	}
 
 }
